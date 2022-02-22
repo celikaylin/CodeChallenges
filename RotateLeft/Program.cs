32814b97ada53481822be18c45117493f14a30e9
celikaylin <mail.aylincelik@gmail.com>
2022-02-22 15:55:05 +0300

    Add project files.

---
+++ RotateLeft/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RotateLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var rotatedList = Solution.RotLeft(list, 1);
+
+            foreach (var item in rotatedList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RotateLeft/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace RotateLeft
+{
+    public class Solution
+    {
+        public static List<int> RotLeft(List<int> a, int d)
+        {
+            int n = a.Count;
+
+            int[] newArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                newArr[(i - d) < 0 ? n + (i - d) : (i - d)] = a[i];
+            }
+
+            a.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                a.Add(newArr[i]);
+            }
+
+            return a;
+
+        }
+    }
+}
